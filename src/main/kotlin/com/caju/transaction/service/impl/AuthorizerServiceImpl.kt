f0c03f4d9cc586628b2cd6e553e7d8e0d33047cd
Racionais158 <jose.bu@ccc.ufcg.edu.br>
2025-02-11 23:19:53 -0300

    ajustes finais

--- src/main/kotlin/com/caju/transaction/service/impl/AuthorizerServiceImpl.kt
+++ src/main/kotlin/com/caju/transaction/service/impl/AuthorizerServiceImpl.kt
@@ -15,12 +15,11 @@ class AuthorizerServiceImpl(
     private val mCCFactoryService: MCCFactoryService
 ): AuthorizerService {
 
-    private val account = AccountUtilsBD.getAccount("123") //todo: buscar em um BD pelo accountId e retornar objeto e seus saldos
-
     override fun authorize(transactionRequest: TransactionRequest): TransactionResponse {
         merchantMCCService.checkingAndUpdateMccByMerchant(transactionRequest)
         val category = CategoryEnum.getBenefitByMCC(transactionRequest.mcc)
         val transactionType = mCCFactoryService.getTransactionType(category)
+        val account = AccountUtilsBD.getAccount(transactionRequest.accountId) //todo: buscar em um BD pelo accountId e retornar objeto e seus saldos
         val status = transactionType.debit(category, account.balances, transactionRequest.totalAmount)
 
         return TransactionResponse(
